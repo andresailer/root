86d7da69aa26a27e6b6f02ddb0d7fd68c249dab1
Andre Sailer <andre.philippe.sailer@cern.ch>
2024-11-25 12:11:33 +0100

    RTD: add basic config

--- documentation/conf.py
+++ documentation/conf.py
@@ -2,6 +2,33 @@ import logging
 import os
 import subprocess
 
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ROOT'
+copyright = '2024, ROOT Developers'
+author = 'ROOT Developers'
+release = 'master'
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
 subprocess.call('conda list', shell=True)
 
 subprocess.call('env', shell=True)
